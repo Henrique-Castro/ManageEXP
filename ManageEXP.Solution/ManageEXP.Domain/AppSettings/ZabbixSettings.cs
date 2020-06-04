4b6a1bf069c47c2bda99a54b2a9eb656f2f376af
Henrique-Castro <henrique.castro@intelitrader.com.br>
2020-06-04 15:11:12 -0300

    BLOCK-1: Estrutura projetos e cria primeiro endpoint

---
+++ ManageEXP.Solution/ManageEXP.Domain/AppSettings/ZabbixSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManageEXP.Domain.AppSettings
+{
+    public class ZabbixSettings
+    {
+        public string Url { get; set; }
+        public string Version { get; set; }
+    }
+}
---
+++ ManageEXP.Solution/ManageEXP.WebApi/Controllers/ItemController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ManageEXP.Domain.Interfaces.Services;
+using ManageEXP.Domain.Services;
+using ManageEXP.Domain.ValueObjects;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManageEXP.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemController : ControllerBase
+    {
+        readonly IItemService _itemService;
+
+        public ItemController(ItemService service)
+        {
+            _itemService = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetItem(ZabbixRequest request)
+        {
+            var result = await _itemService.GetItem(request);
+
+            return Ok(result);
+        }
+    }
+}
