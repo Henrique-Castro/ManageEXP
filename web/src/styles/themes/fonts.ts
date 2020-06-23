60c6f1e534b6eaf091b7188fe75f2acb811960f9
Matheus Santos <matheus.psantos2016@gmail.com>
2020-06-23 11:56:57 -0300

    fonts space added

--- web/src/styles/themes/fonts.ts
+++ web/src/styles/themes/fonts.ts
@@ -1,4 +1,18 @@
 export default {
+    size: {
+        xxs: 8,
+        xs: 10,
+        s: 12,
+        p: 14,
+        xp: 16,
+        xxp: 18,
+        m: 24,
+        g: 42,
+        xg: 48,
+        xxg: 54,
+        gg: 64,
+        xxgg: 40,
+    },
     robotoLightItalic: 'Roboto Light Italic',
     robotoRegular: 'Roboto Regular',
     robotoItalic: 'Roboto Italic',
@@ -6,12 +20,32 @@ export default {
     robotoBlack: 'Roboto Black',
 };
 
+export interface FontsPropsSize {
+    xxs: number;
+    xs: number;
+    s: number;
+    p: number;
+    xp: number;
+    xxp: number;
+    m: number;
+    xm: number;
+    xxm: number;
+    g: number;
+    xg: number;
+    xxg: number;
+    gg: number;
+    xxgg: number;
+};
+
 export interface FontsProps {
+    size: FontsPropsSize;
     robotoLightItalic: string,
     robotoRegular: string,
     robotoItalic: string,
-    robotoBold: string, 
+    robotoBold: string,
     robotoBlack: string,
 };
 
 export type TextOptions = keyof FontsProps;
+
+export type TextUnits = keyof FontsPropsSize;
