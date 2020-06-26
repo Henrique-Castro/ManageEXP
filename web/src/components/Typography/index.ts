a8c70fa5ca73e839f6e9cb48ac36899099d367d6
Matheus Santos <matheus.psantos2016@gmail.com>
2020-06-26 10:56:07 -0300

    index.ts added to export all files

---
+++ web/src/components/Typography/index.ts
@@ -0,0 +1,11 @@
+export { default as Typography } from './BaseTypography';
+
+export { default as Headline } from './Headline';
+
+export { default as Title } from './Title';
+
+export { default as Body1 } from './Body1';
+
+export { default as Body2 } from './Body2';
+
+export { default as Caption } from './Caption'
