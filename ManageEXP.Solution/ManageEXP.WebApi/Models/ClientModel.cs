37c3a558ac753e6d13fa49d79f48441c33c05084
henrique-castro <henrique.castro@intelitrader.com.br>
2020-06-28 17:12:06 -0300

    Adiciona endpoints para registro e consulta de clientes
    Também cria outros serviços para atualização e deleção de clientes além do CRUD de hosts também, porém sem endpoints ainda.

---
+++ ManageEXP.Solution/ManageEXP.WebApi/Models/ClientModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ManageEXP.Domain.Entities;
+
+namespace ManageEXP.WebApi.Models
+{
+    public class ClientModel
+    {
+        public string Company { get; set; }
+        public string TechnicalManager { get; set; }
+        public int DomainId { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public Client ToEntity()
+        {
+            return new Client(Company, TechnicalManager, DomainId, Email, PhoneNumber);
+        }
+    }
+}
