8845b015d5f20674b490ddc3d1b1aca68e3b0476
Matheus Santos <matheus.psantos2016@gmail.com>
2020-06-29 11:33:27 -0300

    exported components updated

--- web/src/components/index.ts
+++ web/src/components/index.ts
@@ -1,3 +1,9 @@
 export { default as Div } from './Div';
 
 export * from './Typography';
+
+export { default as Navigation } from './Navigation';
+
+export { default as Container } from './Container';
+
+export { default as Header } from './Header';
