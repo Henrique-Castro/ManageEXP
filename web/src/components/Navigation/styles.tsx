42817c497819de736633073b0923fa8e1c3b232b
Matheus Santos <matheus.psantos2016@gmail.com>
2020-06-29 11:31:51 -0300

    Navigation updated

--- web/src/components/Navigation/styles.tsx
+++ web/src/components/Navigation/styles.tsx
@@ -1,5 +1,30 @@
 import styled from 'styled-components';
 
-export const Nav = styled.nav`
+export const Nav = styled.div` 
+    height: 100vh;
+    display: flex;
+    flex-direction: column ;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px 28px ;
+    background-color: ${({ theme }) => theme.colors.background.primary};
+    position: fixed;
+    left: 0;
+    top: 0;
 
+    ul {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        li {
+            cursor: pointer;
+            margin-bottom: 16px;
+        };
+    };
+
+    .copy {
+        color: ${({ theme }) => theme.colors.text.white};
+        font-size: 16px;
+    };
 `;
