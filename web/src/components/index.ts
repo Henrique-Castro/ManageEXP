c8811c3512340575734b7d48a4dfc9ca91fa5ccc
Matheus Santos <matheus.psantos2016@gmail.com>
2020-07-01 10:44:10 -0300

    feat: Listas export added

--- web/src/components/index.ts
+++ web/src/components/index.ts
@@ -9,3 +9,5 @@ export { default as Container } from './Container';
 export { default as Header } from './Header';
 
 export { default as Status } from './Status';
+
+export { default as Listas } from './Listas';
