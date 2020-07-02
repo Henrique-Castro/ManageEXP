c65c602fd35846224d1901c4bbefe7057ced4a09
Matheus Santos <matheus.psantos2016@gmail.com>
2020-07-02 09:23:16 -0300

    fix: Container style fixed

--- web/src/components/Status/Domain/styles.tsx
+++ web/src/components/Status/Domain/styles.tsx
@@ -6,18 +6,22 @@ import { ColorsStatusOptions } from "../../../styles/themes/colors";
 const transformToRgba = (variant: ColorsStatusOptions) => {
   if (variant === "publish") {
     return "rgba(11,234,11, 0.2)";
-  }
+  };
   if (variant === "verify") {
     return "rgba(228, 220, 0, 0.2)";
-  }
+  };
   if (variant === "outofarea") {
     return "rgba(234, 15, 11, 0.2)";
-  }
+  };
 };
 
 export const Container = styled.div<StyledProps>`
-  padding: 8px 12px;
-  width: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 12px;
+  border-radius: 8px;
+  width: 120px;
   background-color: ${({ variant }) => transformToRgba(variant)};
 `;
 
