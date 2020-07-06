e3995e99910d1ec99355b84c3e988089980f1a68
Matheus Santos <matheus.psantos2016@gmail.com>
2020-07-06 10:56:18 -0300

    feat: Footer import added

--- web/src/components/Listas/index.tsx
+++ web/src/components/Listas/index.tsx
@@ -1,5 +1,6 @@
 import Cabecalho from "./Cabecalho";
 import Dominio from "./Dominio";
 import Table from './DefaultTable';
+import Footer from './Footer'
 
-export default { Cabecalho, Dominio, Table };
+export default { Cabecalho, Dominio, Table, Footer };
