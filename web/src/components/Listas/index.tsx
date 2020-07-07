8486883409660cf8282fef982bbf446d732890a6
Matheus Santos <matheus.psantos2016@gmail.com>
2020-07-07 11:47:26 -0300

    feat: Listas.Certificado import added

--- web/src/components/Listas/index.tsx
+++ web/src/components/Listas/index.tsx
@@ -1,6 +1,7 @@
 import Cabecalho from "./Cabecalho";
-import Dominio from "./Dominio";
 import Table from './DefaultTable';
 import Footer from './Footer'
+import Dominio from "./Dominio";
+import Certificado from "./Certificado";
 
-export default { Cabecalho, Dominio, Table, Footer };
+export default { Cabecalho, Table, Footer, Dominio, Certificado };
