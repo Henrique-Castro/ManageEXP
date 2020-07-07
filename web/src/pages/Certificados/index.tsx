3903a214ebd3a723351a3175fa1e628f3922039d
Matheus Santos <matheus.psantos2016@gmail.com>
2020-07-07 11:48:02 -0300

    feat: Listas.Certificado added to page

--- web/src/pages/Certificados/index.tsx
+++ web/src/pages/Certificados/index.tsx
@@ -1,14 +1,55 @@
 import React, { useEffect } from 'react';
 
-import { Title } from '../../components';
+import { Title, Listas } from '../../components';
+import { ICertificado } from '../../components/Listas/Certificado';
+
+const header = ["Cliente", "Última Atualização", "Dias Restantes"];
+
+const data: ICertificado[] = [
+    {
+        cliente: "Ifnity.Inc",
+        dataAtualizacao: "18/03/2020",
+        diasRestantes: "70.000"
+    },
+    {
+        cliente: "Ifnity.Inc",
+        dataAtualizacao: "18/03/2020",
+        diasRestantes: "12.000"
+    },
+    {
+        cliente: "Ifnity.Inc",
+        dataAtualizacao: "18/03/2020",
+        diasRestantes: "30.000"
+    },
+    {
+        cliente: "Ifnity.Inc",
+        dataAtualizacao: "18/03/2020",
+        diasRestantes: "43.000"
+    },
+];
 
 function Certificados() {
+
+    let dataTeste = '07/07/2020';
+
+    let date = new Date(dataTeste);
+
+    console.log(date.getMonth());
+
     useEffect(() => {
         document.title = "Certificados | ManageEXP";
     }, []);
 
-    return(
-        <Title text='Certificados' />
+    return (
+        <>
+            <Title text='Certificados' />
+            <Listas.Table>
+                <Listas.Cabecalho headerData={header} />
+                <Listas.Certificado data={data} />
+                <Listas.Footer />
+            </Listas.Table>
+
+        </>
     );
 };
 
