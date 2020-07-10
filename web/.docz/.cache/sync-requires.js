afbdb93d66915a6233a5e7c1f20e3cac7a795167
Matheus Santos <matheus.psantos2016@gmail.com>
2020-07-10 12:09:20 -0300

    feat: docz added

---
+++ web/.docz/.cache/sync-requires.js
@@ -0,0 +1,12 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\_git\\projects\\ManageEXP\\web\\.docz\\.cache\\dev-404-page.js"))),
+  "component---src-docz-index-mdx": hot(preferDefault(require("C:\\_git\\projects\\ManageEXP\\web\\src\\docz\\index.mdx"))),
+  "component---src-pages-404-js": hot(preferDefault(require("C:\\_git\\projects\\ManageEXP\\web\\.docz\\src\\pages\\404.js")))
+}
+
