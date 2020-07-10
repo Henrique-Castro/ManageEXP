4c77c6ab7d680279cb3aaaa97a1f0177da034ee7
Matheus Santos <matheus.psantos2016@gmail.com>
2020-07-10 09:57:48 -0300

    feat: Cliente added

--- web/src/components/Listas/index.tsx
+++ web/src/components/Listas/index.tsx
@@ -3,5 +3,6 @@ import Table from './DefaultTable';
 import Footer from './Footer'
 import Dominio from "./Dominio";
 import Certificado from "./Certificado";
+import Cliente from "./Cliente";
 
-export default { Cabecalho, Table, Footer, Dominio, Certificado };
+export default { Cabecalho, Table, Footer, Dominio, Certificado, Cliente };
