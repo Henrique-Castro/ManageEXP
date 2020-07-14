3f9312839ae2e054e244ced092481869659258ed
Matheus Santos <matheus.psantos2016@gmail.com>
2020-07-14 11:13:26 -0300

    feat: docz pages added

---
+++ web/.docz/.cache/caches/gatsby-plugin-mdx/mdx-scopes-dir/8b81e0bcbef43239653e84c9d909e874.js
@@ -0,0 +1,8 @@
+import { Playground } from 'docz';
+import { Button } from "..\\..\\..\\..\\..\\src\\docz\\components\\index.tsx";
+import * as React from 'react';
+export default {
+  Playground,
+  Button,
+  React
+};
--- web/.docz/doczrc.js
+++ web/.docz/doczrc.js
@@ -4,19 +4,7 @@ export default {
         'Introdução',
         'Inicialização do projeto',
         'Tecnologias usadas',
-        { 
-            name: "Componentes",
-            menu: [
-                'Div',
-                'Typography',
-                'Button',
-                'Header',
-                'Nav',
-                'Listas',
-                'Status',
-                'Form'
-            ], 
-        },
+        'Componentes',
         'Utils',
         'Agradecimentos',
     ],
