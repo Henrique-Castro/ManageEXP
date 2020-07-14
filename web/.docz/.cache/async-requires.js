99714b1c35221977fdd970c187d477a5fd8be038
Matheus Santos <matheus.psantos2016@gmail.com>
2020-07-14 09:25:11 -0300

    feat: technologies page started

--- web/.docz/.cache/async-requires.js
+++ web/.docz/.cache/async-requires.js
@@ -3,8 +3,12 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---cache-dev-404-page-js": () => import("./../../.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-components-button-index-mdx": () => import("./../../../src/components/Button/index.mdx" /* webpackChunkName: "component---src-components-button-index-mdx" */),
   "component---src-docz-about-mdx": () => import("./../../../src/docz/about.mdx" /* webpackChunkName: "component---src-docz-about-mdx" */),
+  "component---src-docz-components-mdx": () => import("./../../../src/docz/components.mdx" /* webpackChunkName: "component---src-docz-components-mdx" */),
   "component---src-docz-index-mdx": () => import("./../../../src/docz/index.mdx" /* webpackChunkName: "component---src-docz-index-mdx" */),
+  "component---src-docz-technologies-mdx": () => import("./../../../src/docz/technologies.mdx" /* webpackChunkName: "component---src-docz-technologies-mdx" */),
+  "component---src-docz-thanks-mdx": () => import("./../../../src/docz/thanks.mdx" /* webpackChunkName: "component---src-docz-thanks-mdx" */),
   "component---src-pages-404-js": () => import("./../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */)
 }
 
--- web/.docz/.cache/sync-requires.js
+++ web/.docz/.cache/sync-requires.js
@@ -6,8 +6,12 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\_git\\projects\\ManageEXP\\web\\.docz\\.cache\\dev-404-page.js"))),
+  "component---src-components-button-index-mdx": hot(preferDefault(require("C:\\_git\\projects\\ManageEXP\\web\\src\\components\\Button\\index.mdx"))),
   "component---src-docz-about-mdx": hot(preferDefault(require("C:\\_git\\projects\\ManageEXP\\web\\src\\docz\\about.mdx"))),
+  "component---src-docz-components-mdx": hot(preferDefault(require("C:\\_git\\projects\\ManageEXP\\web\\src\\docz\\components.mdx"))),
   "component---src-docz-index-mdx": hot(preferDefault(require("C:\\_git\\projects\\ManageEXP\\web\\src\\docz\\index.mdx"))),
+  "component---src-docz-technologies-mdx": hot(preferDefault(require("C:\\_git\\projects\\ManageEXP\\web\\src\\docz\\technologies.mdx"))),
+  "component---src-docz-thanks-mdx": hot(preferDefault(require("C:\\_git\\projects\\ManageEXP\\web\\src\\docz\\thanks.mdx"))),
   "component---src-pages-404-js": hot(preferDefault(require("C:\\_git\\projects\\ManageEXP\\web\\.docz\\src\\pages\\404.js")))
 }
 
