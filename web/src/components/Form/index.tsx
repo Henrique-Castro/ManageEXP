8a17cdab5f6ff0201abd69aedc27684f5e8aa44e
Matheus Santos <matheus.psantos2016@gmail.com>
2020-07-18 18:54:11 -0300

    feat: api connections added

--- web/src/components/Form/index.tsx
+++ web/src/components/Form/index.tsx
@@ -1,13 +1,46 @@
-import React from "react";
+import React, { useState, ChangeEvent, FormEvent } from "react";
 
 import { useAuth } from "../../contexts/auth";
+import { useHistory } from "react-router-dom"
 
 import { Container } from "./styles";
 
 import { Headline, Button } from "..";
 
+import api from "../../services/api";
+
 const Form: React.FC = () => {
   const { signIn } = useAuth();
+  const history = useHistory();
+
+  const [formData, setFormData] = useState({
+    url: "",
+    user: "",
+    password: "",
+  });
+
+  const handleURL = () => {
+    const { url } = formData;
+
+    api.post("zabbix", url);
+  };
+
+  function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+    const { name, value } = event.target;
+
+    setFormData({ ...formData, [name]: value });
+  };
+
+  function handleSumit(event: FormEvent) {
+    const { user, password } = formData;
+
+    const data = {
+      user, 
+      password
+    };
+
+    api.post('Auth', data);
+  }
 
   return (
     <Container>
@@ -18,17 +51,32 @@ const Form: React.FC = () => {
       <form action="">
         <div className="inputContainer">
           <label>URL do Zabbix</label>
-          <input type="text" placeholder="Escreva a URL do Zabbix" />
+          <input
+            type="text"
+            name="url"
+            placeholder="Escreva a URL do Zabbix"
+            onChange={handleInputChange}
+          />
         </div>
 
         <div className="inputContainer">
           <label>Email</label>
-          <input type="email" placeholder="Escreva seu email" />
+          <input 
+            type="email"
+            name="user" 
+            placeholder="Escreva seu email"
+            onChange={handleInputChange} 
+          />
         </div>
 
         <div className="inputContainer">
           <label>Senha</label>
-          <input type="password" placeholder="Escreva sua senha" />
+          <input 
+            type="password"
+            name="password" 
+            placeholder="Escreva sua senha" 
+            onChange={handleInputChange}  
+          />
         </div>
       </form>
       <Button.Default text="Entre!" onClick={() => signIn()} />
